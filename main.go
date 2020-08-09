31a37d8f6ea10b8d744823b15e70301a28847091
lokideos <archmage09@gmail.com>
2020-08-09 17:49:34 +0300

    Refactoring; change var names to go conventional var names

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	nums := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	for _, num := range nums {
-		if num%2 == 0 {
-			fmt.Println(num, "is even")
+	ns := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+	for _, n := range ns {
+		if n%2 == 0 {
+			fmt.Println(n, "is even")
 		} else {
-			fmt.Println(num, "is odd")
+			fmt.Println(n, "is odd")
 		}
 	}
 }
